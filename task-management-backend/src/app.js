ac3d559a2dd23d7d422e08fbd922ed38f195edac
Muhammad Fazeel <muhammadfazeel@Muhammads-MacBook-Pro.local>
2024-11-09 21:02:08 +0300

    preparing sample for demo

--- task-management-backend/src/app.js
+++ task-management-backend/src/app.js
@@ -1,51 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true })
-	.then(() => console.log("connected to db"))
-	.catch(console.error);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch(console.error);
 
-const Task = require('./models/task');
+const Task = require("./models/task");
 
-app.get('/tasks', async (req, res) => {
-	const tasks = await Task.find();
+app.get("/tasks", async (req, res) => {});
 
-	res.json(tasks);
-});
+app.post("/task/new", (req, res) => {});
 
-app.post('/task/new', (req, res) => {
-	const task = new Task({
-		text: req.body.text,
-		priority: req.body.priority
-	})
+app.delete("/task/delete/:id", async (req, res) => {});
 
-	task.save();
+app.put("/task/update/:id", async (req, res) => {});
 
-	res.json(task);
-});
-
-app.delete('/task/delete/:id', async (req, res) => {
-	const result = await Task.findByIdAndDelete(req.params.id);
-
-	res.json({result});
-});
-
-app.put('/task/update/:id', async (req, res) => {
-	const task = await Task.findById(req.params.id);
-
-	task.text = req.body.text;
-
-	Task.save();
-
-	res.json(task);
-});
-
-app.listen(3001);
+app.listen(process.env.PORT);
